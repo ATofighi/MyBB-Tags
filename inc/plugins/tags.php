20b4104e272d919fb65b5fdbeee08bbdffa70345
ATofighi <alirtofighim@gmail.com>
2014-09-18 15:07:00 +0330

    In the name of God

---
+++ inc/plugins/tags.php
@@ -0,0 +1,472 @@
+<?php
+/**
+ * MyBB 1.8
+ * Copyright 2014 My-BB.Ir Group, All Rights Reserved
+ *
+ * Website: http://my-bb.ir
+ * Author: AliReza_Tofighi
+ *
+ */
+
+// Make sure we can't access this file directly from the browser.
+if(!defined("IN_MYBB"))
+{
+	die("This file cannot be accessed directly.");
+}
+	
+/*
+if(defined('THIS_SCRIPT') && THIS_SCRIPT== 'index.php')
+{
+    global $templatelist;
+    if(isset($templatelist) && $templatelist != '')
+    {
+        $templatelist .= ',';
+    }
+	else
+	{
+		$templatelist = '';
+	}
+
+    $templatelist .= 'hello_index';
+}
+*/
+
+$plugins->add_hook("global_start", "tags_global");
+$plugins->add_hook("newthread_start", "tags_newthread");
+$plugins->add_hook("newthread_do_newthread_end", "tags_newthread_done");
+$plugins->add_hook("editpost_end", "tags_editpost");
+$plugins->add_hook("editpost_do_editpost_end", "tags_editpost_done");
+$plugins->add_hook("showthread_start", "tags_showthread");
+
+function tags_info()
+{
+	global $lang;
+	//$lang->load('tags');
+	return array(
+		"name"			=> 'Tags',
+		"description"	=> '',
+		"website"		=> "http://myb-b.ir",
+		"author"		=> "My-BB.Ir Group",
+		"authorsite"	=> "http://my-bb.ir",
+		"version"		=> "1.0",
+		"compatibility" => "18*"
+	);
+}
+
+function tags_activate()
+{
+	if(!function_exists('find_replace_templatesets'))
+	{
+		require_once MYBB_ROOT.'inc/adminfunctions_templates.php';
+	}
+	
+
+	find_replace_templatesets('newthread', '#'.preg_quote('{$posticons}').'#', '{$tags}{$posticons}');
+	find_replace_templatesets('editpost', '#'.preg_quote('{$posticons}').'#', '{$tags}{$posticons}');
+	find_replace_templatesets('showthread', '#'.preg_quote('{$ratethread}').'#', '{$ratethread}{$tags}');
+}
+
+function tags_deactivate()
+{
+	if(!function_exists('find_replace_templatesets'))
+	{
+		require_once MYBB_ROOT.'inc/adminfunctions_templates.php';
+	}
+	
+	find_replace_templatesets('newthread', '#'.preg_quote('{$tags}').'#', '');
+	find_replace_templatesets('editpost', '#'.preg_quote('{$tags}').'#', '');
+	find_replace_templatesets('showthread', '#'.preg_quote('{$tags}').'#', '');
+}
+
+function tags_install()
+{
+	global $db, $lang, $mybb;
+	
+/*	$templatearray = array(
+		"tid" => "NULL",
+		"title" => 'tags_index',
+		"template" => $db->escape_string('
+<br />
+<table border="0" cellspacing="{$theme[\'borderwidth\']}" cellpadding="{$theme[\'tablespace\']}" class="tborder">
+<thead>
+	<tr>
+		<td class="thead">
+			<strong>{$lang->hello}</strong>
+		</td>
+	</tr>
+</thead>
+<tbody>
+	<tr>
+		<td class="tcat">
+			<form method="POST" action="misc.php">
+				<input type="hidden" name="my_post_key" value="{$mybb->post_code}" />
+				<input type="hidden" name="action" value="hello" />
+				{$lang->hello_add_message}: <input type="text" name="message" class="textbox" /> <input type="submit" name="submit" value="{$lang->hello_add}" />
+			</form>
+		</td>
+	</tr>
+	<tr>
+		<td class="trow1">
+		{$messages}
+		</td>
+	</tr>
+</tbody>
+</table>
+<br />'),
+		"sid" => "-1",
+	);
+
+	$db->insert_query("templates", $templatearray);
+
+	// create settings group
+	$insertarray = array(
+		'name' => 'hello', 
+		'title' => 'Test Plugin', 
+		'description' => "Settings for Test Plugin.", 
+		'disporder' => 100,
+		'isdefault' => 0
+	);
+	$gid = $db->insert_query("settinggroups", $insertarray);
+	
+	// add settings
+	$setting = array(
+		"name"			=> "hello_display1",
+		"title"			=> "Display Message Index",
+		"description"	=> "Set to no if you do not want to display the messages on index.",
+		"optionscode"	=> "yesno",
+		"value"			=> 1,
+		"disporder"		=> 1,
+		"gid"			=> $gid
+	);
+	$db->insert_query("settings", $setting);
+	
+	// add settings
+	$setting = array(
+		"name"			=> "hello_display2",
+		"title"			=> "Display Message Postbit",
+		"description"	=> "Set to no if you do not want to display the messages below every post.",
+		"optionscode"	=> "yesno",
+		"value"			=> 1,
+		"disporder"		=> 2,
+		"gid"			=> $gid
+	);
+	$db->insert_query("settings", $setting);*/
+	
+
+	rebuild_settings();
+	
+	// Create our entries table
+	$collation = $db->build_create_table_collation();
+	
+	if(!$db->table_exists("tags"))
+    {
+		$db->write_query("CREATE TABLE `".TABLE_PREFIX."tags` (
+		  `id` int(10) UNSIGNED NOT NULL auto_increment,
+		  `tid` int(100) UNSIGNED NOT NULL default '0',
+		  `name` varchar(200)  NOT NULL default '',
+		  `hash` varchar(200)  NOT NULL default '',
+		  PRIMARY KEY  (`id`)
+			) ENGINE=MyISAM{$collation}");
+	}
+
+	if(!$db->field_exists("tags", "threads"))
+	{
+		$db->write_query("ALTER TABLE ".TABLE_PREFIX."threads ADD `tags` text NOT NULL, ADD `tags_hash` text NOT NULL");
+	}
+	elseif(!$db->field_exists("tags", "tags_hash"))
+	{
+		$db->write_query("ALTER TABLE ".TABLE_PREFIX."threads ADD `tags_hash` text NOT NULL");
+	}
+}
+
+function tags_is_installed()
+{
+	global $db;
+
+	if($db->table_exists("tags"))
+	{
+		return true;
+	}
+
+	return false;
+}
+
+function tags_uninstall()
+{
+	global $db, $mybb;
+	
+/*	$db->delete_query('templates', "title IN ('tags_index') AND sid='-1'");
+
+	$db->delete_query("settinggroups", "name = 'tags'");
+	
+	$db->delete_query('settings', "name IN ('hello_display1', 'hello_display2')");*/
+	
+	rebuild_settings();
+	
+	if($db->table_exists('tags'))
+	{
+		$db->drop_table('tags');
+	}
+	
+	if($db->field_exists("tags", "threads"))
+	{
+		$db->query("ALTER TABLE ".TABLE_PREFIX."threads DROP tags, DROP tags_hash");
+	}
+}
+
+function tags_string2tag($s)
+{
+	$s = my_strtolower($s);
+	$s = ltrim(rtrim(trim($s)));
+	$s = str_replace(array("`","~","!","@","#","$","%","^","&","*","(",")","_","+","-","=","\\","|","]","[","{","}",'"',"'",";",":","/","."," ",">","<"), ",", $s);
+	$s = ltrim(rtrim(trim($s, ','),','),',');
+	return $s;
+}
+
+function tags_global()
+{
+	global $mybb;
+	if($mybb->seo_support)
+	{
+		define('TAG_URL', "tag-{name}.html");
+		define('TAG_URL_PAGE', "tag.html");
+	}
+	else
+	{
+		define('TAG_URL', "tag.php?name={name}");
+		define('TAG_URL_PAGE', "tag.php");
+	}
+}
+
+function get_tag_link($name='')
+{
+	if($name == '')
+	{
+		$link = TAG_URL_PAGE;
+	}
+	else
+	{
+		$link = str_replace("{name}", $name, TAG_URL);
+	}
+	return htmlspecialchars_uni($link);
+}
+
+function tags_newthread()
+{
+	global $mybb, $db, $tags, $tags_value;
+
+	$tags_value = $mybb->get_input('tags');
+	$tags_value = htmlspecialchars_uni(tags_string2tag($tags_value));
+
+	$tags = <<<EOT
+<tr>
+	<td class="trow2" width="20%" valign="top"><strong>Tags:</strong></td>
+	<td class="trow2"><input type="text" class="textbox" name="tags" size="40" maxlength="85" value="{$tags_value}" tabindex="2" id="tags" /></td>
+</tr>
+<script src="{$mybb->asset_url}/jscripts/tags/jquery.tagsinput.js"></script>
+<link rel="stylesheet" type="text/css" href="{$mybb->asset_url}/jscripts/tags/jquery.tagsinput.css" />
+<script type="text/javascript">
+	$("#tags").tagsInput({
+		'height': '40px',
+		'width': 'auto',
+		'defaultText': 'Add tags here'
+	});
+
+	$("#tags").on('change', function()
+	{
+		$(this).importTags($(this).val());
+	});
+</script>
+EOT;
+}
+
+function tags_newthread_done()
+{
+	global $mybb, $db, $tid;
+
+	$tags_value = $mybb->get_input('tags');
+	$tags_value = tags_string2tag($tag_value);
+	$tags = explode(',', $tags_value);
+	$subject = $mybb->get_input('subject');
+	$subject = tags_string2tag($subject);
+	$subject = explode(',', $subject);
+
+	$tags = $tags + $subject;
+	$tags_value = implode(',', $tags);
+
+	$tags_insert = array();
+	foreach($tags as $tag)
+	{
+		if($tag != '')
+		{
+			$tags_hash_arr[] = md5(my_strtolower($tag));
+			array_push($tags_insert, array(
+				'tid' => $tid,
+				'name' => $tag,
+				'hash' => md5(my_strtolower($tag))
+			));
+		}
+	}
+
+	$tags_hash = implode(',', $tags_hash_arr);
+
+	$db->insert_query_multiple("tags", $tags_insert);
+	$db->update_query("threads", array(
+		"tags" => ','.$tags_value.',',
+		"tags_hash" => ','.$tags_hash.','
+	), "tid='{$tid}'", 1);
+}
+
+function tags_editpost()
+{
+	global $mybb, $db, $thread, $post, $tags, $tags_value;
+
+	if($thread['firstpost'] != $post['pid'])
+	{
+		return;
+	}
+
+	$tags_value = $mybb->get_input('tags');
+	if(!$tags_value)
+	{
+		$tags_value = $thread['tags'];
+	}
+	$tags_value = htmlspecialchars_uni(tags_string2tag($tags_value));
+
+	$tags = <<<EOT
+<tr>
+	<td class="trow2" width="20%" valign="top"><strong>Tags:</strong></td>
+	<td class="trow2"><input type="text" class="textbox" name="tags" size="40" maxlength="85" value="{$tags_value}" tabindex="2" id="tags" /></td>
+</tr>
+<script src="{$mybb->asset_url}/jscripts/tags/jquery.tagsinput.js"></script>
+<link rel="stylesheet" type="text/css" href="{$mybb->asset_url}/jscripts/tags/jquery.tagsinput.css" />
+<script type="text/javascript">
+	$("#tags").tagsInput({
+		'height': '40px',
+		'width': 'auto',
+		'defaultText': 'Add tags here'
+	});
+
+	$("#tags").on('change', function()
+	{
+		$(this).importTags($(this).val());
+	});
+</script>
+EOT;
+}
+
+function tags_editpost_done()
+{
+	global $mybb, $db, $tid, $thread, $post;
+
+	if($thread['firstpost'] != $post['pid'])
+	{
+		return;
+	}
+
+	$tags_value = $mybb->get_input('tags');
+	$tags_value = tags_string2tag($tags_value);
+	$tags_hash_arr = array();
+	$tags = explode(',', $tags_value);
+	$subject = $mybb->get_input('subject');
+	$subject = tags_string2tag($subject);
+	$subject = explode(',', $subject);
+
+	$tags = $tags + $subject;
+	$tags_value = implode(',', $tags);
+
+	$tags_insert = array();
+	foreach($tags as $tag)
+	{
+		$tags_hash_arr[] = md5(my_strtolower($tag));
+		array_push($tags_insert, array(
+			'tid' => $tid,
+			'name' => $tag,
+			'hash' => md5(my_strtolower($tag))
+		));
+	}
+
+	$tags_hash = implode(',', $tags_hash_arr);
+
+	$db->delete_query("tags", "tid='{$tid}'");
+	$db->insert_query_multiple("tags", $tags_insert);
+	$db->update_query("threads", array(
+		"tags" => ','.$tags_value.',',
+		"tags_hash" => ','.$tags_hash.','
+	), "tid='{$tid}'", 1);
+}
+
+function tags_showthread()
+{
+	global $mybb, $db, $theme, $thread, $tags;
+	$subject = $thread['subject'];
+	$tid = $thread['tid'];
+
+	$thread['tags'] = trim(ltrim(rtrim($thread['tags'],','),','),',');
+	if($thread['tags'] == '')
+	{
+		$subject = str_replace(array(" ", "-", "_"), ',', $subject);
+		$subject = ltrim(rtrim(trim($subject)));
+		$subject = ltrim(rtrim(trim($subject, ','), ','), ',');
+		$tags = explode(',', $subject);
+		$tags_hash_arr = array();
+
+		$tags_insert = array();
+		foreach($tags as $tag)
+		{
+			$tags_hash_arr[] = md5(my_strtolower($tag));
+			array_push($tags_insert, array(
+				'tid' => $tid,
+				'name' => $tag,
+				'hash' => md5(my_strtolower($tag))
+			));
+		}
+
+		$tags_hash = implode(',', $tags_hash_arr);
+
+		$db->delete_query("tags", "tid={$tid}");
+		$db->insert_query_multiple("tags", $tags_insert);
+		$db->update_query("threads", array(
+			"tags" => ','.$subject.',',
+			"tags_hash" => ','.$tags_hash.','
+		), "tid='{$tid}'", 1);
+		$thread['tags'] = ','.$subject.',';
+		$thread['tags_hash'] = ','.$tags_hash.',';
+	}
+
+	$tags = '';
+	$thread['tags'] = explode(',', $thread['tags']);
+	$comma = '';
+	$i = 0;
+	foreach($thread['tags'] as $tag)
+	{
+		if($tag == '' || $i >= 25)
+			continue;
+
+		$tag = htmlspecialchars_uni($tag);
+		$tag_link = get_tag_link($tag);
+		$tags .= <<<EOT
+{$comma}<a href="{$mybb->settings['bburl']}/{$tag_link}" title="{$tag}">{$tag}</a> 
+EOT;
+		$comma = ', ';
+		$i++;
+	}
+
+	$tags = <<<EOT
+<br class="clear" />
+<table border="0" cellspacing="{$theme['borderwidth']}" cellpadding="{$theme['tablespace']}" class="tborder tfixed clear">
+	<tr>
+		<td class="thead">
+			Tags
+		</td>
+	</tr>
+	<tr>
+		<td class="trow1">
+			{$tags}
+		</td>
+	</tr>
+</table>
+<br class="clear" />
+		
+EOT;
+}
---
+++ tag.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * MyBB 1.8
+ * Copyright 2014 My-BB.Ir Group, All Rights Reserved
+ *
+ * By: AliReza_Tofighi
+ * Website: http://my-bb.ir
+ *
+ */
+
+define('IN_MYBB', 1);
+define('NO_ONLINE', 1);
+define('THIS_SCRIPT', 'index.php');
+
+$templatelist = "";
+
+require_once './global.php';
+require_once MYBB_ROOT.'inc/functions_forumlist.php';
+require_once MYBB_ROOT.'inc/class_parser.php';
+$parser = new postParser;
+$parser_options = array();
+$parser_options['allow_html'] = 0;
+$parser_options['allow_mycode'] = 1;
+$parser_options['allow_smilies'] = 1;
+$parser_options['allow_imgcode'] = 0;
+$parser_options['allow_videocode'] = 0;
+$parser_options['me_username'] = 0;
+$parser_options['filter_badwords'] = 1;
+
+$dir = 'left';
+$no_dir = 'right';
+if($lang->settings['rtl'])
+{
+	$dir = 'right';
+	$no_dir = 'left';
+}
+
+// get forums user cannot view
+$unviewable = get_unviewable_forums(true);
+if($unviewable)
+{
+	$unviewwhere = " AND fid NOT IN ($unviewable)";
+	$tunviewwhere = " AND thread.fid NOT IN ($unviewable)";
+}
+else
+{
+	$unviewwhere = '';
+}
+
+// get inactive forums
+$inactive = get_inactive_forums();
+if($inactive)
+{
+	$inactivewhere = " AND fid NOT IN ($inactive)";
+	$tinactivewhere = " AND thread.fid NOT IN ($inactive)";
+}
+else
+{
+	$inactivewhere = '';
+}
+
+$name = $mybb->get_input('name');
+$name = $parser->parse_badwords($name);
+$name = ltrim(rtrim(trim($name)));
+$name = my_strtolower($name);
+$name = htmlspecialchars_uni($name);
+$hash = md5($name);
+add_breadcrumb('Tags', get_tag_link());
+
+$tag_link = get_tag_link();
+
+if(!$name)
+{
+
+	$tag = <<<EOT
+	<html>
+		<head>
+			<title>{$mybb->settings['bbname']} - Tags</title>
+			{$headerinclude}
+		</head>
+		<body>
+			{$header}
+			<form action="{$tag_link}" method="get">
+			<table border="0" cellspacing="{$theme['borderwidth']}" cellpadding="{$theme['tablespace']}" class="tborder clear">
+				<tr>
+					<td class="thead" colspan="10">
+						Search
+					</td>
+				</tr>
+				<tr>
+					<td class="trow1">
+						<input type="text" class="textbox" placeholder="Please enter a tag..." name="name" style="width:100%;box-sizing:border-box;padding:5px 8px;font-size:16px;" />
+					</td>
+					<td class="trow1" width="50">
+						<input type="submit" class="button" style="width:100%;box-sizing:border-box;padding:5px 8px;font-size:16px;" value="Go" />
+					</td>
+				</tr>
+			</table>
+			</form>
+			{$footer}
+		</body>
+	</html>
+EOT;
+	output_page($tag);
+}
+else
+{
+	add_breadcrumb($name, get_tag_link($name));
+
+	$query = $db->query("SELECT COUNT(thread.tid) as numrows from `".TABLE_PREFIX."tags` tag
+						 LEFT JOIN `".TABLE_PREFIX."threads` thread on(tag.tid = thread.tid)
+						 WHERE tag.hash = '{$hash}' And thread.tid > 0 and thread.visible='1'{$tunviewwhere}{$tinactivewhere} AND thread.closed NOT LIKE 'moved|%'
+						 limit 1");
+	$nums = $db->fetch_array($query);
+	$count = $nums['numrows'];
+	$page = $mybb->get_input('page', 1);
+	$pages = $count / 10;
+	$pages = ceil($pages);
+
+	if($page > $pages || $page <= 0)
+	{
+		$page = 1;
+	}
+
+	if($page)
+	{
+		$start = ($page-1) * 10;
+	}
+	else
+	{
+		$start = 0;
+		$page = 1;
+	}
+
+	$multipage = multipage($count, 10, $page, get_tag_link($name));
+
+	$query = $db->query("SELECT thread.tid, post.message, post.username, post.uid, thread.subject, thread.views, thread.replies from `".TABLE_PREFIX."tags` tag
+						 LEFT JOIN `".TABLE_PREFIX."threads` thread on(tag.tid = thread.tid)
+						 LEFT JOIN `".TABLE_PREFIX."posts` post on(thread.firstpost = post.pid)
+						 WHERE tag.hash = '{$hash}' And thread.tid > 0 And post.pid > 0 and thread.visible='1'{$tunviewwhere}{$tinactivewhere} AND thread.closed NOT LIKE 'moved|%'
+						 GROUP BY thread.tid
+						LIMIT {$start}, 10");
+
+	$tags = '';
+	while($tag = $db->fetch_array($query))
+	{
+		if(!$tag['tid'])
+			continue;
+
+
+		if($mybb->seo_support == true)
+		{
+			$highlight = "?highlight=".urlencode($name);
+		}
+		else
+		{
+			$highlight = "&amp;highlight=".urlencode($name);
+		}
+
+		$tag['message'] = my_substr($tag['message'], 0, 500);
+		$tag['message'] = $parser->parse_message($tag['message'], $parser_options);
+		$tag['subject'] = htmlspecialchars_uni($parser->parse_badwords($tag['subject']));
+		$tag['threadlink'] = get_thread_link($tag['tid']);
+		$tag['profilelink'] = build_profile_link($tag['username'], $tag['uid']);
+		$tags .= <<<EOT
+	<tr>
+		<td class="tcat" colspan="10">
+			<div class="float_{$no_dir}">
+				By: {$tag['profilelink']} - Replies: <a href="javascript:MyBB.whoPosted({$tag['tid']});">{$tag['replies']}</a> - Views: {$tag['views']}
+			</div>
+			<a href="{$tag['threadlink']}{$highlight}"><strong>{$tag['subject']}</strong></a>
+		</td>
+	</tr>
+	<tr>
+		<td class="trow1" colspan="10">
+			<div style="max-height:100px;overflow:auto">
+				{$tag['message']}
+			</div>
+		</td>
+	</tr>
+EOT;
+	}
+
+	if($tags == '')
+	{
+		error("We can't find anything for this Tag");
+	}
+
+	$tag = <<<EOT
+	<html>
+		<head>
+			<title>Tags - {$name}</title>
+			{$headerinclude}
+		</head>
+		<body>
+			{$header}
+			<form action="{$tag_link}" method="get">
+			<table border="0" cellspacing="{$theme['borderwidth']}" cellpadding="{$theme['tablespace']}" class="tborder clear">
+				<tr>
+					<td class="thead" colspan="10">
+						Tags
+					</td>
+				</tr>
+				<tr>
+					<td class="trow1">
+						<input type="text" class="textbox" name="name" style="width:100%;box-sizing:border-box;padding:5px 8px;font-size:16px;" value="{$name}" />
+					</td>
+					<td class="trow1" width="50">
+						<input type="submit" class="button" style="width:100%;box-sizing:border-box;padding:5px 8px;font-size:16px;" value="Go" />
+					</td>
+				</tr>
+				{$tags}
+			</table>
+			</form>
+			{$multipage}
+			{$footer}
+		</body>
+	</html>
+EOT;
+	output_page($tag);
+}
