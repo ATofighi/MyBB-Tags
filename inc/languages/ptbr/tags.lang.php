6ee10629f52fc52fe72007584921f96c05cf08e6
dthiago <thiagoalves@live.com>
2014-09-19 23:12:18 -0300

    Create tags.lang.php

---
+++ inc/languages/ptbr/tags.lang.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * MyBB-Tags 1.8 Pacote de Língua Brasileira
+* Author: AliReza_Tofighi - http://my-bb.ir
+ * Direitos Autorais 2014 My-BB.Ir Group, Todos os Direitos Reservados
+ * Traduzido por dthiago ArnoldLayne-xXx http://bf4brasil.com.br/
+ * 
+ *
+ */
+
+
+$l['tags'] = "Etiquetas";
+$l['tags_placeholder'] = "Adicionar Etiqueta";
+$l['tags_search'] = "Buscar";
+$l['tags_search_placeholder'] = "Adicione uma etiqueta";
+$l['tags_go'] = "Vai";
+$l['tags_author'] = "Autor";
+$l['tags_replies'] = "Respostas";
+$l['tags_views'] = "Visualizações";
+$l['tags_notags'] = "Desculpe não há retorno redefina a sua etiqueta e tente novamente.";
+$l['tags_disabled'] = 'Desculpe mas as etiquetas estão desabilitadas.';
